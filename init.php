4a41f438223716a3ff5fca6eb6eaaab91530f78f
pavel <pavel@DELL-PC.LAN-GATE>
2021-04-11 16:40:37 +0500

    add a file init example

---
+++ init.php
@@ -0,0 +1,32 @@
+<?
+
+require_once 'ClassHotModifier.php';
+
+// add a single class file
+// ClassHotModifier::setClasses(['CCrmDeal'], 'modules/crm/classes/mysql/crm_deal.php');
+// ClassHotModifier::setClasses(['Bitrix\Crm\Requisite\EntityLink'], 'modules/crm/lib/requisite/entitylink.php');
+// ClassHotModifier::setClass(CCrmEntityListBuilder::class,'modules/crm/classes/general/crm_entity_list_builder.php');
+
+
+
+//add  a multi class file
+/*
+ClassHotModifier::setClassesExt(
+		[
+			'CCrmRestService',
+			'ICrmRestProxy',
+			'CCrmRestEventDispatcher',
+			'CCrmLiveFeedMessageRestProxy',
+		], [
+			'CCrmRequisiteRestProxy',
+			'CCrmRequisiteLinkRestProxy',
+			'CCrmRequisiteBankDetailRestProxy',
+			'CCrmAddressRestProxy',
+			'CCrmDealRestProxy',
+			'CCrmContactRestProxy',
+			'CCrmCompanyRestProxy',
+			'CCrmProductRestProxy',
+			'CCrmProductRowRestProxy',
+		], 'modules/crm/classes/general/restservice.php');
+*/
+
